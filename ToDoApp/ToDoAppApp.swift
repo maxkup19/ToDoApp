2b5d2a58165601822e0c4157ff8685533db19981
Maksym Kupchenko <maxkup19@gmail.com>
2022-08-16 13:23:16 +0200

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Max Kup on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
